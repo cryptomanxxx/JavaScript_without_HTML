59309ca3de75d3497931b2b3303caea91bbd0e0b
cryptomanxxx <48676920+cryptomanxxx@users.noreply.github.com>
2019-08-22 00:47:00 +0200

    Update JavaS.js
    user defined functions

--- JavaS.js
+++ JavaS.js
@@ -1,36 +1,46 @@
-function process(e) {
+function countline(element) {
+  var text = $(element).val();
+  var lines = text.split("\n");
+  var count = 0;
+  for (var i = 0; i < lines.length - 1; i++) {
+    if (lines[i].trim() != "" && lines[i].trim() != null) {
+      count += 1;
+    }
+  }
+  return count + 1;
+}
+
+function get(e) {
   var code = (e.keyCode ? e.keyCode : e.which);
   if (code == 13) { //Enter keycode
-    run();
+    var input = document.getElementById('inputbox');
+    var output = eval(input.value);
+    count = countline(document.getElementById('inputbox'));
+    console.log("Calculation " + "number " + count + " = " + output);
+    document.getElementById("outputbox").innerHTML += output + '\n';
   }
 }
 
-function run() {
-  var input = document.getElementById('inputbox');
-
-  var lines = input.value.split('\n')
-  var results = [];
-
-  var parser = math.parser();
-  for (var i = 0; i < lines.length; i++) {
-    var q = trim(lines[i])
-    if (q.length > 0) {
-      try {
-        results.push('= ' + math.format(parser.eval(q)));
-      }
-      catch (err) {
-        results.push(err.toString());
-      }
-    }
-    else {
-      results.push("");
-    }
+// Gives you an array with data from a to b
+function seq(a, b) {
+  var data = [];
+  for (var i = a; i <= b; i++) {
+    data.push(i);
   }
-
-  var output = results.join('\n')  
-  $('#outputbox').val(output);
+  return data;
 }
 
-function trim(str) {
-  return str.replace(/^\s+|\s+$/g, '');
+// Calculates the sum of a given array a
+function sum(a) {
+  var z = 0;
+  for (var i = 0; i < a.length; i++) {
+    z = z + a[i];
+  }
+  return z;
 }
+
+// clears the input and output box
+function clear() {
+  document.getElementById('inputbox').value = "";
+  document.getElementById('outputbox').value = "";
+};

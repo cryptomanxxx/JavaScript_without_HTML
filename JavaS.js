2b09f472e666bf2326d5549d1fd6fb35f607ab88
cryptomanxxx <48676920+cryptomanxxx@users.noreply.github.com>
2019-08-24 10:32:30 +0200

    Update JavaS.js
    added plot function

--- JavaS.js
+++ JavaS.js
@@ -17,17 +17,39 @@ function parse(e) {
   }
 }
 
-// clear the input and output boxes
+// clears the input and output boxes
 function clear() {
   event.preventDefault();
   input = "";
   output = "";
   document.getElementById('inputbox').value = "";
   document.getElementById('outputbox').value = "";
+  document.getElementById('mydiv').innerHTML = "";
   return "";
 };
 
-// gives you an array with data from a to b
+// an array with random numbers between -1 and 1
+function rand(n) {
+  x = [];
+  for (var i = 0; i < n; i++) {
+    x[i] = Math.random() * 2 - 1;
+  }
+  console.log("x = " + x);
+  return x;
+}
+
+// an array with a random walk 
+function rw(n) {
+  var x = [];
+  x[0] = 100;
+  for (var i = 1; i <= n; i++) {
+    x[i] = x[i - 1] + (Math.random() * 2 - 1);
+  }
+  console.log("x = " + x);
+  return x;
+}
+
+// an array with data from a to b
 function seq(a, b) {
   var data = [];
   for (var i = a; i <= b; i++) {
@@ -43,4 +65,49 @@ function sum(a) {
     z = z + a[i];
   }
   return z;
+}
+
+// plots a give data array 
+function plot(z) {
+  var yy = z;
+  var xx = [];
+
+  for (var i = 0; i <= yy.length; i++) {
+    xx[i] = JSON.stringify(i);
+  }
+
+  var data = [{
+    x: xx,
+    y: yy,
+    type: 'scatter',
+    line: { color: 'green', width: 2 }
+  }];
+
+  var layout =
+  {
+    paper_bgcolor: 'lightgrey',
+    plot_bgcolor: 'lightgrey',
+    margin: { l: 60, b: 60, r: 20, t: 20 },
+    title: "",
+    xaxis: {
+      title: 'x-axis', titlefont: {
+        family: 'Courier New, monospace', size: 18,
+        color: 'black'
+      }
+    },
+    yaxis: {
+      title: 'y-axis', titlefont: { family: 'Courier New, monospace', size: 18, color: 'black' },
+      width: 1000, height: 380,
+      xaxis: {
+        tickfont: { size: 12, color: 'black' }, showgrid: true, tickmode: "linear",
+        gridcolor: 'black', linecolor: 'black'
+      },
+      yaxis: {
+        tickfont: { size: 12, color: 'black' }, showgrid: true,
+        gridcolor: 'black', linecolor: 'black'
+      }
+    }
+  };
+  Plotly.newPlot('mydiv', data, layout, { displayModeBar: false, staticPlot: true });
+  return "plot";
 }

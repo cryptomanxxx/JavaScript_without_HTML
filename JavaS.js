6a53a198d034f0a9122d408817c5881db49cde3e
cryptomanxxx <48676920+cryptomanxxx@users.noreply.github.com>
2019-09-04 12:18:37 +0200

    Update JavaS.js
    row counter

--- JavaS.js
+++ JavaS.js
@@ -1,27 +1,48 @@
+// first row cursor position in textarea inputbox on page load
 function setfocus() {
   var input = document.getElementById('inputbox');
   input.focus();
 }
 
+// counts the number of times the user push enter
+function increment() {
+  increment.n = increment.n || 0;
+  console.log("increment.n = " + increment.n);
+  return ++increment.n;
+
+}
+
 function parse(e) {
   var key = window.event.keyCode;
   if (key == 13) { //keycode for enter 
-    input = document.getElementById('inputbox');
-    output = eval(input.value);
+
+    var input = document.getElementById('inputbox');
+    var output = eval(input.value);
+
+    var cc = increment();
+    console.log("cc = " + cc);
     var out = document.getElementById("outputbox").value
     if (out == "" || out.length == 0 || out == null) {
+      document.getElementById("count1").value += eval(cc + 1) + '\n';
+      document.getElementById("count2").value = eval(cc);
       document.getElementById("outputbox").value += output;
     } else {
+      document.getElementById("count1").value += eval(cc + 1) + '\n';
+      document.getElementById("count2").value += '\n' + eval(cc);
       document.getElementById("outputbox").value += '\n' + output;
     }
   }
+
+  if (cc == 0) { document.getElementById("count2").value = "" };
+
 }
 
 // clears the input and output boxes
 function clear() {
   event.preventDefault();
-  input = "";
-  output = "";
+  increment.n = -1;
+  document.getElementById('count1').value = "";
+  document.getElementById('count2').value = "";
   document.getElementById('inputbox').value = "";
   document.getElementById('outputbox').value = "";
   document.getElementById('mydiv').innerHTML = "";
@@ -45,7 +66,6 @@ function rw(n) {
   for (var i = 1; i <= n; i++) {
     x[i] = x[i - 1] + (Math.random() * 2 - 1);
   }
-  console.log("x = " + x);
   return x;
 }
 
@@ -67,8 +87,23 @@ function sum(a) {
   return z;
 }
 
+function help() {
+  document.getElementById('mydiv').innerHTML =
+    "1) Function rand(n) gives you a 1D array with length n with random numbers between -1 and 1." + "<br>" +
+    "2) Function rw(n) gives you a 1D array with length n with a pure random walk." + "<br>" +
+    "3) Function seq(a,b) gives you a 1D array with data from a to b." + "<br>" +
+    "4) Function sum(a) gives you a sum of a 1D array a." + "<br>" +
+    "5) Function plot(z) gives you a plot of a 1D array z." + "<br>" +
+    "6) Function clear() gives you a clean input-, output- and plot- box plus restarts row counters." + "<br>" +
+    "7) Function help() gives you command help."
+  return "help";
+}
+
 // plots a give data array 
 function plot(z) {
+
+  document.getElementById('mydiv').innerHTML = "";
+
   var yy = z;
   var xx = [];
 
@@ -108,6 +143,7 @@ function plot(z) {
       }
     }
   };
+
   Plotly.newPlot('mydiv', data, layout, { displayModeBar: false, staticPlot: true });
   return "plot";
 }

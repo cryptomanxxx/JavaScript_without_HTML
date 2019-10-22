000a7e38d30bf9a64ae7be1d87fe01d1cd6c986a
cryptomanxxx <48676920+cryptomanxxx@users.noreply.github.com>
2019-10-22 17:19:17 +0200

    more functions plus split js file

--- JavaS1.js
+++ JavaS1.js
@@ -9,7 +9,6 @@ function CreateInputDiv() {
   increment();
   cc = increment.n;
   console.log("increment.n = " + cc);
-
   input = document.createElement("div");
   input.setAttribute("id", "input" + cc);
   input.setAttribute("class", "input");
@@ -81,11 +80,10 @@ function parse(e1, e2) {
       // no colon and no # = display output and create new lines
       else {
         CreateOutputDiv();
-        // calculate and assign output value to output div  
-
-        // check if input contains a matrix input      
+        // calculate and assign output value to output div     
         var outz = eval(inz);
-        if (inz.indexOf('=') == -1 && inz.indexOf('matrix') == -1) {
+        // check if input contains a matrix input   
+        if (inz.indexOf('matrix') == -1) {
           output.innerHTML = outz;
         }
         CreateInputDiv();
@@ -114,36 +112,7 @@ function reparse(inz) {
   return outz != undefined ? true : false;
 }
 
-// gives you help regarding different functions
-function help() {
-  var x =
-    "1) Function help() gives you command help" + "<br>" +
-    "2) Function round(x,z) rounds a number, 1D array or a 2D array x to z decimal points" + "<br>" +
-    "3) Function array(z) returns a javascript array from function's parameters z" + "<br>" +
-    "4) Function arrayMult(d1,d2) multiply two arrays d1 and d2" + "<br>" +
-    "5) Function rand(n) returns a 1D array with length n with random numbers between -1 and 1" + "<br>" +
-    "6) Function rw(n) returns a 1D array with length n with a pure random walk" + "<br>" +
-    "7) Function seq(a,b) gives you a 1D array with data from a to b" + "<br>" +
-    "8) Function sum(a) gives you a sum of a 1D array a" + "<br>" +
-    "9) Function count(a,b) counts the number of elements b in array a. If parameter b is not specified then the count of a is return" + "<br>" +
-    "10) Function ticker() gives you the ticker symbols for the 100 crypto currencies with the largest market cap" + "<br>" +
-    "11) Function time(w) converts a unix timestamp w to a date string " + "<br>" +
-    "12) Function crypto(ticker) gives you historial crypto currency price data for a specified ticker symbol string" + "<br>" +
-    "13) Function plot(z) gives you a plot of a 1D array z." + "<br>" +
-    "14) Function clear() gives you a clean workspace" + "<br>" +
-    "15) Function save(x) where x is a file name that ends with .html in enclosed in a string will save a copy of the current workspace locally" + "<br>" +
-    "16) Function load() loads a html workspace file from a previous session" + "<br>" +
-    "17) Function matrix(z) creates and displays a html table from a 2D array z and returns z" + "<br>" +
-    "18) Function matrixInv(m) calculates the inverse of matrix m with gaussian elimination " + "<br>" +
-    "19) Function matrixMult(a,b) multiplies two matrices a and b" + "<br>" +
-    "20) Function matrixId(n) returns an identity matrix with n number of rows and columns" + "<br>" +
-    "Please note that an input that ends with : hiddes output from view" + "<br>" +
-    "Please note that an input that starts with # is defined as text";
-  return x;
-}
-
-
-// rounds a number, a 1D or a 2D array array a to z decimal points
+// rounds a number, a 1D or a 2D array array x to z decimal points
 function round(x, z) {
   if (z == undefined) { z = 2; }
   console.log("type of = " + typeof (x));
@@ -163,9 +132,12 @@ function round(x, z) {
 
 // returns a javascript array from a function's parameters (arguments)
 function array() {
-  var a = Array.from(arguments);
-  console.log(a);
-  return a;
+  var data = Array.from(arguments);
+  console.log(data);
+  // var a = document.createElement('table');
+  // a.setAttribute("class", "array");
+  output.innerHTML = '[' + data + ']';
+  return data;
 }
 
 // multiply two arrays 
@@ -209,17 +181,6 @@ function seq(a, b) {
   }
   console.log(data);
   return data;
-}
-
-// calculates the sum of a given array a
-function sum(a) {
-  var z = 0;
-  for (var i = 0; i < a.length; i++) {
-    z = z + a[i];
-  }
-  var zz = round(z, 2);
-  console.log(zz);
-  return zz;
 }
 
 // counts the number of elements b in a given array a
@@ -364,16 +325,12 @@ function destroyClickedElement(event) {
 function load() {
   var input = document.createElement('input');
   input.type = 'file';
-
   input.onchange = e => {
-
     // getting a hold of the file reference
     var file = e.target.files[0];
-
     // setting up the reader
     var reader = new FileReader();
     reader.readAsText(file, 'UTF-8');
-
     // here we tell the reader what to do when it's done reading...
     reader.onload = readerEvent => {
       var content = readerEvent.target.result; // this is the content!
@@ -383,144 +340,4 @@ function load() {
   }
   input.click();
   return "workspace loaded";
-}
-
-
-// displays a html table and returns a 2D array
-function matrix(z) {
-
-  var table = document.createElement('table');
-  table.setAttribute("class", "matrix");
-  var tableBody = document.createElement('tbody');
-
-  z.forEach(function (rowData) {
-    var row = document.createElement('tr');
-
-    rowData.forEach(function (cellData) {
-      var cell = document.createElement('td');
-      cell.appendChild(document.createTextNode(cellData));
-      row.appendChild(cell);
-    });
-
-    tableBody.appendChild(row);
-  });
-
-  table.appendChild(tableBody);
-  console.log(z);
-  output.innerHTML = table.outerHTML;
-  return z;
-}
-
-// calculates the inverse of matrix m with gaussian elimination 
-function matrixInv(m) {
-  //if the matrix isn't square: exit (error)
-  if (m.length !== m[0].length) { return; }
-
-  //create the identity matrix (I), and a copy (C) of the original
-  var i = 0, ii = 0, j = 0, dim = m.length, e = 0, t = 0;
-  var I = [], C = [];
-  for (i = 0; i < dim; i += 1) {
-    // Create the row
-    I[I.length] = [];
-    C[C.length] = [];
-    for (j = 0; j < dim; j += 1) {
-
-      //if we're on the diagonal, put a 1 (for identity)
-      if (i == j) { I[i][j] = 1; }
-      else { I[i][j] = 0; }
-
-      // Also, make the copy of the original
-      C[i][j] = m[i][j];
-    }
-  }
-
-  // Perform elementary row operations
-  for (i = 0; i < dim; i += 1) {
-    // get the element e on the diagonal
-    e = C[i][i];
-
-    // if we have a 0 on the diagonal (we'll need to swap with a lower row)
-    if (e == 0) {
-      //look through every row below the i'th row
-      for (ii = i + 1; ii < dim; ii += 1) {
-        //if the ii'th row has a non-0 in the i'th col
-        if (C[ii][i] != 0) {
-          //it would make the diagonal have a non-0 so swap it
-          for (j = 0; j < dim; j++) {
-            e = C[i][j];       //temp store i'th row
-            C[i][j] = C[ii][j];//replace i'th row by ii'th
-            C[ii][j] = e;      //repace ii'th by temp
-            e = I[i][j];       //temp store i'th row
-            I[i][j] = I[ii][j];//replace i'th row by ii'th
-            I[ii][j] = e;      //repace ii'th by temp
-          }
-          //don't bother checking other rows since we've swapped
-          break;
-        }
-      }
-      //get the new diagonal
-      e = C[i][i];
-      //if it's still 0, not invertable (error)
-      if (e == 0) { return }
-    }
-
-    // Scale this row down by e (so we have a 1 on the diagonal)
-    for (j = 0; j < dim; j++) {
-      C[i][j] = C[i][j] / e; //apply to original matrix
-      I[i][j] = I[i][j] / e; //apply to identity
-    }
-
-    // Subtract this row (scaled appropriately for each row) from ALL of
-    // the other rows so that there will be 0's in this column in the
-    // rows above and below this one
-    for (ii = 0; ii < dim; ii++) {
-      // Only apply to other rows (we want a 1 on the diagonal)
-      if (ii == i) { continue; }
-
-      // We want to change this element to 0
-      e = C[ii][i];
-
-      // Subtract (the row above(or below) scaled by e) from (the
-      // current row) but start at the i'th column and assume all the
-      // stuff left of diagonal is 0 (which it should be if we made this
-      // algorithm correctly)
-      for (j = 0; j < dim; j++) {
-        C[ii][j] -= e * C[i][j]; //apply to original matrix
-        I[ii][j] -= e * I[i][j]; //apply to identity
-      }
-    }
-  }
-  console.log(I);  // C should be the identity and matrix I should be the inverse:
-  return matrix(round(I, 2));
-}
-
-// multiplies two matricies
-// test with m1=[[4,7],[2,6]] m2=[[0.6,-0.7],[-0.2,0.4]] matrixMult(m1,m2) should be [[1,0],[0,1]]
-function matrixMult(a, b) {
-  var aNumRows = a.length, aNumCols = a[0].length,
-    bNumRows = b.length, bNumCols = b[0].length,
-    m = new Array(aNumRows);  // initialize array of rows
-  for (var r = 0; r < aNumRows; ++r) {
-    m[r] = new Array(bNumCols); // initialize the current row
-    for (var c = 0; c < bNumCols; ++c) {
-      m[r][c] = 0;             // initialize the current cell
-      for (var i = 0; i < aNumCols; ++i) {
-        m[r][c] += a[r][i] * b[i][c];
-      }
-    }
-  }
-  var d = round(m, 2);
-  return matrix(d);
-}
-
-// creates an identity matrix with n number of rows and columns
-function matrixId(n) {
-  var data = Array.from(Array(n), () => new Array(n));
-  for (var i = 0; i < n; i++) {
-    for (var j = 0; j < n; j++) {
-      if (i === j) { data[i][j] = 1; }
-      else { data[i][j] = 0; }
-    }
-  }
-  return matrix(data);
 }
